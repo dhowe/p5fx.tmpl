34bd3d704b1d58044e2237bce093c4911a835b19
dhowe <dhowe@cims.nyu.edu>
2024-04-05 02:08:06 +0800

    add text

--- sketch.js
+++ sketch.js
@@ -3,8 +3,11 @@ let done = false;
 function setup() {
 
   createCanvasFx(800, 800, 'mysketch');
-
+  background(245);
+  text('p5fx.tmpl', 350, 400);
+  
   // ... setup code
+  
 }
 
 function draw() {
